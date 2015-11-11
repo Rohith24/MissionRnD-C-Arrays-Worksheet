c5998cb6418b9a3d50351ab1d006db6bc3388b6d
Rohith24 <rohithreddy24@hotmail.com>
2015-11-11 14:08:31 +0530

    Submit Commit

--- src/sortedArrayPositionsChange.cpp
+++ src/sortedArrayPositionsChange.cpp
@@ -12,8 +12,45 @@ NOTES:
 */
 
 #include <stdio.h>
-
+void poschange(int *Arr, int len);
+void swap(int *Arr, int pos1, int pos2){
+	int temp = Arr[pos1];
+	Arr[pos1] = Arr[pos2];
+	Arr[pos2] = temp;
+}
 void * sortedArrayPositionsChange(int *Arr, int len)
 {
-	return NULL;
+	if (Arr == NULL || len < 2)
+		return NULL;
+	else
+	{
+		poschange(Arr, len);
+		
+		return Arr;
+	}
+}
+
+void poschange(int *Arr,int len){
+	int pos1 = -1, pos2 = -1;
+	int flag1 = 0, flag2 = 0,i;
+	for (i = 0; i < len - 1; i++){
+		if (Arr[i + 1] < Arr[i] && flag1 != 1){
+
+			pos1 = i;
+			flag1 = 1;
+		}
+		else if (Arr[i + 1] < Arr[i] && flag2 != 1 && flag1 == 1){
+
+			pos2 = i + 1;
+			flag2 = 1;
+		}
+	}
+
+	if (flag2 == 0){
+		pos2 = pos1 + 1;
+		flag2 = 1;
+	}
+	if (flag1 && flag2){
+		swap(Arr, pos1, pos2);
+	}
 }
