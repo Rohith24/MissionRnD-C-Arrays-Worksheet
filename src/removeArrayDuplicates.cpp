686fbb05796cd84cb7180f02b5ac3ca2e81bc704
Rohith24 <rohithreddy24@hotmail.com>
2015-11-11 21:27:43 +0530

    Submit Commit

--- src/removeArrayDuplicates.cpp
+++ src/removeArrayDuplicates.cpp
@@ -13,7 +13,55 @@ NOTES: Don't create new array, try to change the input array.
 
 #include <stdio.h>
 
+void deletedup(int *Arr, int *len);
+void deletepos(int *Arr, int *len, int pos);
+void sortArray(int *Arr, int len);
+void swap(int *a, int *b);
 void * removeArrayDuplicates(int *Arr, int len)
 {
-	return NULL;
+	if (Arr == NULL || len < 0)
+		return NULL;
+	else
+	{
+		sortArray(Arr, len);
+		deletedup(Arr, &len);
+	}
+}
+
+void sortArray(int *Arr, int len){
+	int counter1 = 0, counter2 = 0;
+	for (counter1 = 0; counter1 < len - 1; counter1++){
+		for (counter2 = counter1 + 1; counter2 < len; counter2++){
+			if (Arr[counter1] > Arr[counter2]){
+				swap(&Arr[counter1],&Arr[counter2]);
+			}
+		}
+	}
+}
+
+void swap(int *a, int *b){
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void deletedup(int *Arr,int *len)
+{
+	int p, q;
+	for (p = 0; p < *len - 1; p++){
+		for (q = p + 1; q < *len;){
+			if (Arr[p] == Arr[q])
+				deletepos(Arr, len, q);
+			else
+				q++;
+		}
+	}
+}
+
+void deletepos(int *Arr, int *len,int p)
+{
+	int i;
+	for (i = p; i<*len - 1; i++)
+		Arr[i] = Arr[i + 1];
+	*len-=1;
 }
